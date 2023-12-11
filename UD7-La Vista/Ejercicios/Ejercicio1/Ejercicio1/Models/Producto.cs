94794373750818f75743392ad1abb809b27669cc
Martin <martin.garcia.figueira@ciclosmontecastelo.com>
2023-12-11 13:49:59 +0100

    Tema7

--- UD7-La Vista/Ejercicios/Ejercicio1/Ejercicio1/Models/Producto.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Ejercicio1.Models
-{
-    public class Producto
-    {
-        public int Id { get; set; }
-        public string Nombre { get; set; }
-        public decimal Precio { get; set; }
-    }
-}
